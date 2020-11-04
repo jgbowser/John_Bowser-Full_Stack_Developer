2bf4be761c8ddf4c644bc52f8cf6cd5e5bf900fd
John Bowser <john.bow7720@gmail.com>
2020-11-03 23:12:59 -0700

    brought in react-spring library, added basic fade in to test

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -1,12 +1,17 @@
 import './Hero.css'
+import { useSpring, animated } from 'react-spring'
 
 const Hero = () => {
+  const slider = useSpring({ opacity: 1, from: { opacity: 0}})
+
   return (
     <section className="Hero">
-      <div className='Hero__content'>
-        <h1 className='Hero__title'>John Bowser</h1>
-        <h2>Full Stack Developer</h2>
-      </div>
+      <animated.div className='Hero__background' style={slider}>
+        <div className='Hero__content'>
+          <h1 className='Hero__title'>John Bowser</h1>
+          <h2 className='Hero__subtitle'>Full Stack Developer</h2>
+        </div>
+      </animated.div>
     </section>
   )
 }
