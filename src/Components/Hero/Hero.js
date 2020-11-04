d532d15f903df617db4bc269e99a60d5f9829cd0
John Bowser <john.bow7720@gmail.com>
2020-11-04 10:48:48 -0700

    good first pass at hero animations

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -2,14 +2,28 @@ import './Hero.css'
 import { useSpring, animated } from 'react-spring'
 
 const Hero = () => {
-  const slider = useSpring({ opacity: 1, from: { opacity: 0}})
+  const fadeIn = useSpring({ opacity: 1, from: { opacity: 0}, delay: 250, config: {duration: 250}})
+  const fromTop = useSpring({
+    transform: 'translate3d(0%,0,0)',
+    opacity: 1,
+    from: { transform: 'translate3d(0,-100%,0)', opacity: 0},
+    delay: 500,
+    config: { duration: 300 }
+  })
+  const fromBottom = useSpring({
+    transform: 'translate3d(0%,0,0)',
+    opacity: 1,
+    from: { transform: 'translate3d(0,100%,0)', opacity: 0},
+    delay: 800,
+    config: { duration: 300 }
 
+  })
   return (
     <section className="Hero">
-      <animated.div className='Hero__background' style={slider}>
+      <animated.div className='Hero__background' style={fadeIn}>
         <div className='Hero__content'>
-          <h1 className='Hero__title'>John Bowser</h1>
-          <h2 className='Hero__subtitle'>Full Stack Developer</h2>
+          <animated.h1 className='Hero__title' style={fromTop}>John Bowser</animated.h1>
+          <animated.h2 className='Hero__subtitle' style={fromBottom}>Full Stack Developer</animated.h2>
         </div>
       </animated.div>
     </section>
