5331b4507edd199621cbf48d9e29a3c3c829b7d8
John Bowser <john.bow7720@gmail.com>
2020-11-04 20:37:30 -0700

    adjusted header styling for cleaner look

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,15 +1,13 @@
 import Hero from '../Hero/Hero'
 import Header from '../Header/Header'
-import { useSpring, animated } from 'react-spring'
 import './App.css';
 
 function App() {
-  const fadeIn = useSpring({ opacity: 1, from: { opacity: 0 }, delay: 900, config: {duration: 250}})
   return (
     <div className='App'>
-      <animated.header style={fadeIn}>
+      <header>
         <Header />
-      </animated.header>
+      </header>
       <main>
         <Hero />
       </main>
