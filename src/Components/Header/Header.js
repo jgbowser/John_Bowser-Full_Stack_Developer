67cbc568f1995f37bab7f4ec3f91b4382b143a4f
John Bowser <john.bow7720@gmail.com>
2020-11-24 11:17:44 -0700

    media queries and styling to handle main header at smaller screen sizes

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,11 @@
+import { useState } from 'react'
 import { useSpring, animated } from 'react-spring'
 import { Link } from 'react-scroll'
 import { Link as RouterLink } from 'react-router-dom'
 import './Header.css'
 
 const Header = () => {
+  const [ showNav, setShowNav ] = useState(false)
 
   const fadeIn = useSpring({
     opacity: 1,
@@ -21,6 +23,14 @@ const Header = () => {
     from: { opacity: 0 }
   })
 
+  function toggleNav(e) {
+    if(e.key === 'Enter' || e.key === ' ') {
+      setShowNav(!showNav)
+    } else if(e.type === 'click'){
+      setShowNav(!showNav)
+    }
+  }
+
   return (
     <animated.div className='Header__container' style={fadeIn}>
       <div className='Header__title'>
@@ -28,16 +38,26 @@ const Header = () => {
           JGB
         </animated.p>
       </div>
-      <nav className='Header__links'>
+        <div 
+          className='Header__burger-container'
+          onClick={toggleNav}
+          onKeyDown={(e) => toggleNav(e)}
+          tabIndex='0'
+        >
+          <span className={`Header__burger ${showNav ? 'show' : ''}`}></span>
+        </div>
+      <nav className={`Header__links ${showNav ? 'show' : ''}`}>
         <ul>
           <animated.li style={fadeNav}>
             <Link 
               to='about'
+              className={`${showNav ? 'show' : ''}`}
               activeClass='active'
               spy={true}
               smooth={true}
               duration={500}
               tabIndex='0'
+              onClick={toggleNav}
               >
               About
             </Link>
@@ -45,11 +65,13 @@ const Header = () => {
           <animated.li style={fadeNav}>
             <Link 
               to='projects'
+              className={`${showNav ? 'show' : ''}`}
               activeClass='active'
               spy={true}
               smooth={true}
               duration={500}
               tabIndex='0'
+              onClick={toggleNav}
             >
               Projects
             </Link>
@@ -57,11 +79,13 @@ const Header = () => {
           <animated.li style={fadeNav}>
             <Link 
               to='contact'
+              className={`${showNav ? 'show' : ''}`}
               activeClass='active'
               spy={true}
               smooth={true}
               duration={500}
               tabIndex='0'
+              onClick={toggleNav}
             >
               Contact
             </Link>
@@ -69,6 +93,7 @@ const Header = () => {
           <animated.li style={fadeNav}>
             <RouterLink
               to='/blog'
+              className={`${showNav ? 'show' : ''}`}
             >
               Blog
             </RouterLink>
