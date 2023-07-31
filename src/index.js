6b316e8e6e35ecf83a44039f0bd915e7d4772c0b
John Bowser <john.bow7720@gmail.com>
2023-07-30 18:25:47 -0600

    under construction

--- src/index.js
+++ src/index.js
@@ -1,21 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
-import './index.css';
-import App from './Components/App/App';
-import AllPosts from './Components/AllPosts/AllPosts'
-import BlogPost from './Components/BlogPost/BlogPost'
-
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import "./index.css";
+import App from "./Components/App/App";
+import AllPosts from "./Components/AllPosts/AllPosts";
+import BlogPost from "./Components/BlogPost/BlogPost";
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Switch>
-      <Route exact path='/' component={App} />
-      <Route path='/blog' component={AllPosts} />
-      <Route path='/:slug' component={BlogPost} />
-    </Switch>
-  </BrowserRouter>,
-  document.getElementById('root')
+  // <BrowserRouter>
+  //   <Switch>
+  //     <Route exact path='/' component={App} />
+  //     <Route path='/blog' component={AllPosts} />
+  //     <Route path='/:slug' component={BlogPost} />
+  //   </Switch>
+  // </BrowserRouter>,
+  <h1 style={{ textAlign: "center" }}>UNDER RECONSTRUCTION, CHECK BACK LATER</h1>,
+  document.getElementById("root")
 );
-
-
